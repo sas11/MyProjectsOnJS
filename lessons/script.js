dd26ac6a56cc8d3d693a5905dc14e398e91c4577
Aliaksandr <sanya2798@gmail.com>
2022-08-03 14:43:17 +0300

    up lessons folder, new progress

---
+++ lessons/script.js
@@ -0,0 +1,50 @@
+'use strict';
+                    //start my training.
+// console.log(1); 
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// console.log(3/0); //infinity
+// console.log('string' * 9);                  //NaN -  not a number
+
+// let und;
+// console.log(und); //undefined
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+// const answer = +prompt('You have 18 old?', '18');        // prompt always return !!!type string!!!  
+                                                            // + unar plus change type on a number
+// console.log(answer+5);
+
+// const answers = [];
+// answers[0] = prompt('What your name?', '');
+// answers[1] = prompt('What your lastName?', '');
+// answers[2] = prompt('What your old?', '');
+
+// document.write(answers);
+// console.log(typeof(null));
+
+                        //intopolation
+
+// const category = 'toys';
+// console.log('https://someurl.com/' + category);
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'alex';
+// alert(`Hello ${user}`);
+
+                        //Operators
+
+// console.log('arr' + ' - Object');
+// console.log(4 + ' - Object');
+
+// let incr = 10,
+//     decr = 10;
+
+//     ++incr;//11
+//     --decr;//9
+// console.log(incr++);//11
+// console.log(decr++);//9
