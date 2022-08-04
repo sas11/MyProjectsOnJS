f5adf439eb04c1820587caf8ae93faa544af0e7d
Aliaksandr <sanya2798@gmail.com>
2022-08-04 23:00:27 +0300

    up lessons

--- lessons/script.js
+++ lessons/script.js
@@ -1,5 +1,5 @@
 'use strict';
-                    //start my training.
+//start my training.
 // console.log(1); 
 
 // let number = 5;
@@ -16,7 +16,7 @@
 // const result = confirm('Are you here?');
 // console.log(result);
 // const answer = +prompt('You have 18 old?', '18');        // prompt always return !!!type string!!!  
-                                                            // + unar plus change type on a number
+// + unar plus change type on a number
 // console.log(answer+5);
 
 // const answers = [];
@@ -27,7 +27,7 @@
 // document.write(answers);
 // console.log(typeof(null));
 
-                        //intopolation
+//intopolation
 
 // const category = 'toys';
 // console.log('https://someurl.com/' + category);
@@ -36,7 +36,7 @@
 // const user = 'alex';
 // alert(`Hello ${user}`);
 
-                        //Operators
+//Operators
 
 // console.log('arr' + ' - Object');
 // console.log(4 + ' - Object');
@@ -48,3 +48,49 @@
 //     --decr;//9
 // console.log(incr++);//11
 // console.log(decr++);//9
+
+// if else switch
+
+// if (4 == 9) {
+//     console.log('Ok!');
+// } else {
+//     console.log('error');
+// }
+
+//  const num = 50;
+// // if(num <49){
+// //     console.log('error');
+// // }
+// // else if( num >100){
+// //     console.log('many');
+// // }else{
+// //     console.log('Ok');
+// // }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+// switch(num){
+//     case 49:
+//         console.log('none');
+//         break;
+//     case 100:
+//         console.log('none');
+//         break;
+//     case 50:
+//         console.log('ok');
+//         break;
+//     default:
+//         break;
+// }
+// const lines = 5;
+// let result = '';
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = i; j < 5; j++) {
+//         result += ' ';
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     console.log(result);
+//     result = '';
+// }
