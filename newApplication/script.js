9c3f65d8bb32c42e905a19bde9b3272e2528888d
Aliaksandr <sanya2798@gmail.com>
2022-08-06 17:32:13 +0300

    Add functions in newApp

--- newApplication/script.js
+++ newApplication/script.js
@@ -1,5 +1,15 @@
  'use strict';
- const numberOfFilms = +prompt('How many movies have you watched?', '');
+ let numberOfFilms;
+
+ function start() {
+    numberOfFilms = +prompt('How many movies have you watched?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+       numberOfFilms = +prompt('How many movies have you watched?', '');
+    }
+ }
+
+ start();
 
  const personaMovieDB = {
     count: numberOfFilms,
@@ -9,34 +19,54 @@
     privat: false
  }
 
- 
-let lastMovie,
-    ratingMovie,
-    answer=0;
+ function rememberMyFilms() {
+    let lastMovie,
+       ratingMovie,
+       answer = 0;
 
-while(answer!=2){
-   lastMovie = prompt('Last move watched?', '');
-   ratingMovie = prompt('And it rating?', '');
-   
-   if(lastMovie != null && ratingMovie != null && lastMovie != '' &&ratingMovie != '' && lastMovie.length <50){
-      personaMovieDB.movies[lastMovie] = ratingMovie;
-      alert('done!');
-      answer++;
-   }else{
-      alert('your answers incorrect, repeat please.');
+    while (answer != 2) {
+       lastMovie = prompt('Last move watched?', '');
+       ratingMovie = prompt('And it rating?', '');
+
+       if (lastMovie != null && ratingMovie != null && lastMovie != '' && ratingMovie != '' && lastMovie.length < 50) {
+          personaMovieDB.movies[lastMovie] = ratingMovie;
+          alert('done!');
+          answer++;
+       } else {
+          alert('your answers incorrect, repeat please.');
+       }
+    }
+ }
+
+ rememberMyFilms();
+
+ function detectPersonalLevel() {
+    if (personaMovieDB.count < 10) {
+       alert('Watched very small moves');
+    } else if (personaMovieDB.count >= 10 && personaMovieDB.count <= 30) {
+       alert('Very good ');
+    } else if (personaMovieDB.count > 30) {
+       alert('Crazy man');
+    } else {
+       alert('error');
+    }
+
+ }
+
+ detectPersonalLevel();
+
+function showMyDB(hidden){
+   if(!hidden){
+      console.log(personaMovieDB);
    }
 }
 
-if(personaMovieDB.count < 10){
-   alert('Watched very small moves');
-}else if(personaMovieDB.count >=10 && personaMovieDB.count <=30){
-   alert('Very good ');
-}else if(personaMovieDB.count >30){
-   alert('Crazy man');
-}else{
-   alert('error');
+showMyDB(personaMovieDB.privat);
+ 
+function writeYourGenres(){
+   for(let i = 0;i<=2;i++){
+      personaMovieDB.genres[i] = prompt(`Your liked genres number ${i+1} ?`, '');
+   }
 }
-   
 
-
-console.dir(personaMovieDB);
+writeYourGenres();
