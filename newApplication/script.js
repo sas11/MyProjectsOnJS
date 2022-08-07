8d008fc8601fdf5439259069666d391d6c2d96dc
Aliaksandr <sanya2798@gmail.com>
2022-08-07 20:38:57 +0300

    up newApp 'rework'

--- newApplication/script.js
+++ newApplication/script.js
@@ -1,73 +1,74 @@
  'use strict';
- let numberOfFilms;
-
- function start() {
-    numberOfFilms = +prompt('How many movies have you watched?', '').trim();
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-       numberOfFilms = +prompt('How many movies have you watched?', '');
-    }
- }
-
- start();
 
  const personaMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actor: {},
     genres: [],
-    privat: false
- }
+    privat: false,
+    start: function () {
+       personaMovieDB.count = +prompt('How many movies have you watched?', '');
 
- function rememberMyFilms() {
-    let lastMovie,
-       ratingMovie,
-       answer = 0;
-
-    while (answer != 2) {
-       lastMovie = prompt('Last move watched?', '').trim();
-       ratingMovie = prompt('And it rating?', '').trim();
-
-       if (lastMovie != null && ratingMovie != null && lastMovie != '' && ratingMovie != '' &&
-        lastMovie.length < 50) {
-          personaMovieDB.movies[lastMovie] = ratingMovie;
-          console.log('done');
-          answer++;
-       } else {
-          alert('your answers incorrect, repeat please.');
+       while (personaMovieDB.count == '' || personaMovieDB.count == null || isNaN(personaMovieDB.count)) {
+          personaMovieDB.count = +prompt('How many movies have you watched?', '');
        }
+    },
+    rememberMyFilms: function () {
+       let lastMovie,
+          ratingMovie,
+          answer = 0;
+
+       while (answer != 2) {
+          lastMovie = prompt('Last move watched?', '');
+          ratingMovie = prompt('And it rating?', '');
+
+          if (lastMovie != null && ratingMovie != null && lastMovie != '' && ratingMovie != '' &&
+             lastMovie.length < 50) {
+             personaMovieDB.movies[lastMovie] = ratingMovie;
+             console.log('done');
+             answer++;
+          } else {
+             console.log('your answers incorrect, repeat please.');
+          }
+       }
+    },
+    detectPersonalLevel: function () {
+       if (personaMovieDB.count < 10) {
+          console.log('Watched very small moves');
+       } else if (personaMovieDB.count >= 10 && personaMovieDB.count <= 30) {
+          console.log('Very good ');
+       } else if (personaMovieDB.count > 30) {
+          console.log('Crazy man');
+       } else {
+          console.log('error');
+       }
+
+    },
+    showMyDB: function (hidden) {
+       if (!hidden) {
+          console.log(personaMovieDB);
+       }
+    },
+    toggleVisibleMyDB: function () {
+       if (!personaMovieDB.privat) {
+          personaMovieDB.privat = true;
+       } else {
+          personaMovieDB.privat = false;
+       }
+    },
+    writeYourGenres: function () {
+       for (let i = 1; i < 2; i++) {
+          let genres = prompt(`Write your liked genres across ', '`, '').toLowerCase();
+          if (genres != null && genres != '') {
+             personaMovieDB.genres = genres.split(', ');//string to array
+             personaMovieDB.genres.sort();
+          } else {
+            console.log('Incorrect data');
+             i--;
+          }
+       }
+       personaMovieDB.genres.forEach((val, index) => {
+          console.log(`Genre #${index+1} - the ${val}`);
+       });
     }
- }
-
- rememberMyFilms();
-
- function detectPersonalLevel() {
-    if (personaMovieDB.count < 10) {
-       alert('Watched very small moves');
-    } else if (personaMovieDB.count >= 10 && personaMovieDB.count <= 30) {
-       alert('Very good ');
-    } else if (personaMovieDB.count > 30) {
-       alert('Crazy man');
-    } else {
-       alert('error');
-    }
-
- }
-
- detectPersonalLevel();
-
-function showMyDB(hidden){
-   if(!hidden){
-      console.log(personaMovieDB);
-   }
-}
-
-showMyDB(personaMovieDB.privat);
- 
-function writeYourGenres(){
-   for(let i = 0;i<=2;i++){
-      personaMovieDB.genres[i] = prompt(`Your liked genres number ${i+1} ?`, '');
-   }
-}
-
-writeYourGenres();
+ };
