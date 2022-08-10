85f88b4e7c157f0085558370509f660113ed3bd8
Aliaksandr <sanya2798@gmail.com>
2022-08-10 19:43:51 +0300

    rework 'project'

--- project/js/script.js
+++ project/js/script.js
@@ -24,3 +24,27 @@ const movieDB = {
     ]
 };
 
+const promo = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+promo.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'ДРАМА';
+
+poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i+1}. ${film}
+        <div class="delete"></div>
+    </li>
+    `;
+});
