6b4940b323991f77b002d9d131bbf83db8ead561
Aliaksandr <sanya2798@gmail.com>
2022-08-10 22:04:07 +0300

    UP lesson 'DOM'

--- lessons/script.js
+++ lessons/script.js
@@ -617,3 +617,23 @@
 
 //     console.log(event.target);
 // });
+
+//DOM
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentElement);
+
+
+// console.log(document.querySelector('[data-current = "3"]').nextElementSibling);
+
+for(let node of document.body.childNodes){
+    if(node.nodeName =='#text'){
+        continue;
+    }
+    console.log(node);
+}
