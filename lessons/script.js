9fbc822d73e7ad6db6180c1ea9ff3d925421ff50
Aliaksandr <sanya2798@gmail.com>
2022-08-14 12:19:59 +0300

    up lesson 'mobil events'

--- lessons/script.js
+++ lessons/script.js
@@ -449,7 +449,7 @@
 //     return str;
 // }
 
-// availableCurr([...baseCurrencies,...additionalCurrencies], 'CNY'); // SPREAD оператор копирования!!! оператор разворота!!!
+// availableCurr([...baseCurrencies,...additionalCurrencies], 'CNY'); // SPREAD оператор копирования!!! оператор разворота!!
 
 //create Objects OOP
 
@@ -773,3 +773,37 @@
 
 // console.log(factorial(5.2));
 
+//Mobil browse
+
+//touchstart
+//touchmove
+//touchend
+//touchenter
+//touchleave
+//touchcancel
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const box = document.querySelector('.box');
+
+//     box.addEventListener('touchstart', (e) => {
+//         e.preventDefault();
+
+//         console.log(e.targetTouches[0].pageX);
+//     });
+
+    // box.addEventListener('touchmove', (e) => {
+    //     e.preventDefault();
+
+    //     console.log('Move');
+    // });
+    
+    // box.addEventListener('touchend', (e) => {
+    //     e.preventDefault();
+
+    //     console.log('End');
+    // });
+// });
+
+//touches
+//targetTouches
+//changedTouches
