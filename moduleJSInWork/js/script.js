7cc745212b5753c465d39ca0947d1481ec217f03
Aliaksandr <sanya2798@gmail.com>
2022-08-14 13:56:42 +0300

    up newModule 'ClassList'

---
+++ moduleJSInWork/js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+//ClassList
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+// console.log(btns[1].classList.add('red', 'sfas'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue')); //добавляет класс если нету и напротив.
+
+// if(btns[1].classList.contains('red')){ //проверяет наличие класса
+//     console.log('red');
+// }
+
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')){
+    //     btns[1].classList.add('red');
+    // }else{
+    //     btns[1].classList.remove('red');
+    // }
+
+    btns[1].classList.toggle('red');
+});
+
+// console.log(btns[0].className);
+
+wrapper.addEventListener('click', (event) => {
+    if(event.target && event.target.matches('button.red')) { //тег содержит селектор, делегирование событий
+        console.log('Hello');
+    }
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
