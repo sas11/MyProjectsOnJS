494e2f34246d19596c2a816608cf161ee5f60a98
Aliaksandr <sanya2798@gmail.com>
2022-08-15 21:28:03 +0300

    UP module 'setTimeout/Interval'

--- moduleJSInWork/js/script.js
+++ moduleJSInWork/js/script.js
@@ -40,3 +40,48 @@
 // const btn = document.createElement('button');
 // btn.classList.add('red');
 // wrapper.append(btn);
+
+//SetTimeout SetInterval
+// const btn = document.querySelector('.btn');
+// let timerId,
+//     i = 0;
+
+
+// function myAnimation() {
+//     const elem = document.querySelector('.box');
+//     let pos = 0;
+
+//     const id = setInterval(frame,10);
+
+//     function frame() {
+//         if(pos == 300){
+//             clearInterval(id);
+//         }else{
+//             pos++;
+//             elem.style.top = `${pos}px`;
+//             elem.style.left = `${pos}px`;
+//         }
+//     }
+// }
+
+// btn.addEventListener('click', myAnimation);
+// btn.addEventListener('click', () => {
+//     timerId = setInterval(logger, 500);
+
+// });
+
+// const text = 'hello';
+// const timerId = setTimeout(logger,2000,text); //функция которая запустится через установленое время
+
+// function logger() {
+//     if(i===3){
+//         clearInterval(timerId);
+//     }
+//     console.log('hello');
+//     i++;
+// }
+
+// let id = setTimeout(function log(){//рекурсия сет таймаута
+//     console.log('hello');
+//     id = setTimeout(log, 500);
+// },500);
