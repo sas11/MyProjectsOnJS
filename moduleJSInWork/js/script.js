ae392d576c95ae75f2e362c9fb2268ae0e8ed906
Aliaksandr <sanya2798@gmail.com>
2022-08-17 22:34:31 +0300

    UP module 'MAP'

--- moduleJSInWork/js/script.js
+++ moduleJSInWork/js/script.js
@@ -85,3 +85,64 @@
 //     console.log('hello');
 //     id = setTimeout(log, 500);
 // },500);
+
+//Map
+
+// const user = {
+//     name: 'Alex',
+//     surname: 'Smith',
+//     birthday: '20/04/1993',
+//     showMyPublicData: function() {
+//         console.log(`${this.name} ${this.surname}`);
+//     }
+// }
+
+// const userMap = new Map(Object.entries(user));
+// console.log(userMap);
+
+// const newUserObject = Object.fromEntries(userMap);
+// console.log(newUserObject);
+
+// const shops = [
+//     {rice: 500},
+//     {oil: 200},
+//     {bread: 50}
+// ];
+
+// const budget = [5000, 15000, 25000];
+
+// const map = new Map([
+//     [{paper: 400}, 8000]
+// ]);
+
+// shops.forEach((shops,i) =>{
+//     map.set(shops,budget[i]);
+// });
+
+// map.set(shops[0], 5000);
+// map.set(shops[1], 15000);
+// map.set(shops[2], 25000);
+
+// console.log(map);
+// console.log(map.get(shops[0]));
+// console.log(map.has(shops[0]));
+// map.delete(key);
+// map.clear();
+// map.size;
+// map.keys()
+
+// const goods = [];
+// for(let shop of map.keys()){
+//     goods.push(Object.keys(shop)[0]);
+// }
+// console.log(goods);
+
+// for(let price of map.values()){
+// }
+// for(let [shop,price] of map.entries()){
+//     console.log(shop,price);
+// }
+
+// map.forEach((value,key,map) => {
+//     console.log(key,value);
+// });
