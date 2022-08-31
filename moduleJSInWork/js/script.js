766ade9f6b78db4a6907bf0e65049dfe996ec569
Aliaksandr <sanya2798@gmail.com>
2022-08-31 19:38:35 +0300

    UP module 'classes'

--- moduleJSInWork/js/script.js
+++ moduleJSInWork/js/script.js
@@ -440,3 +440,39 @@
 // const double = a => a * 2;
 
 // console.log(double(4));
+
+//Classes
+
+// class Rectangle {
+//     constructor(height, width) {
+//         this.height = height;
+//         this.width = width;
+//     }
+
+//     calcArea() {
+//         return this.height * this.width;
+//     }
+// }
+
+// class ColoredRectangleWithText extends Rectangle {
+//     constructor(height, width, text, bgColor) {
+//         super(height, width);
+//         this.text = text;
+//         this.bgColor = bgColor;
+//     }
+
+//     showMyProps() {
+//         console.log(`text: ${this.text}, color: ${this.bgColor}`);
+//     }
+// }
+
+// const div = new ColoredRectangleWithText(25, 10, 'hello world', 'red');
+
+// div.showMyProps();
+// console.log(div.calcArea());
+
+// const square = new Rectangle(5, 5);
+// const long = new Rectangle(7, 8);
+
+// console.log(long.calcArea());
+// console.log(square.calcArea());
