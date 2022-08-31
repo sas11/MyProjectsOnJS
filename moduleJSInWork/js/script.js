e8a60682f18ae4c627950156c891f859d73b4aaa
Aliaksandr <sanya2798@gmail.com>
2022-08-31 18:24:38 +0300

    UP module 'MutationObserver'

--- moduleJSInWork/js/script.js
+++ moduleJSInWork/js/script.js
@@ -310,3 +310,17 @@ console.log(now);
 // }
 
 // setClock('.timer',endTime);
+
+//MutationObserver
+
+const box = document.querySelector('.box');
+
+let observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords);
+});
+
+observer.observe(box, {
+    childList: true
+});
+
+observer.disconnect();
