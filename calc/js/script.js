51149d2af5fb1e0f36269c789d87249589aab758
Aliaksandr <sanya2798@gmail.com>
2022-09-04 22:29:02 +0300

    calc 'old request'

---
+++ calc/js/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const inputRub = document.querySelector('#rub'),
+      inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+    const request = new XMLHttpRequest(); //запрос
+
+    // request.open(method, url, async, login, pass); 
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if( request.status == 200) {
+            
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        }else{
+            inputUsd.value = 'что-то пошло не так';
+        }
+    });
+
+    // status
+    // statusText
+    // response
+    // readyState
+
+});
