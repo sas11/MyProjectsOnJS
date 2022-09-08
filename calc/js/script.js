94ecb6a2e2b7928d1a48c562d0341005ff5dc639
Aliaksandr <sanya2798@gmail.com>
2022-09-08 17:15:56 +0300

    new theme 'server-request'

--- calc/js/script.js
+++ calc/js/script.js
@@ -1,29 +1,73 @@
 'use strict';
 
-const inputRub = document.querySelector('#rub'),
-      inputUsd = document.querySelector('#usd');
+const rub = document.querySelector('#rub'),
+      usd = document.querySelector('#usd');
 
-inputRub.addEventListener('input', () => {
-    const request = new XMLHttpRequest(); //запрос
 
-    // request.open(method, url, async, login, pass); 
-    request.open('GET', 'js/current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
+      rub.addEventListener('input', () => {
+        
+        const request = new XMLHttpRequest();
+        request.open('GET', 'js/current1.json');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.send();
 
-    request.addEventListener('load', () => {
-        if( request.status == 200) {
+        request.addEventListener('load', () => {
+
+            if(request.status===200){
+                const data = JSON.parse(request.response);
+
+                usd.value = (+rub.value/data.current.usd).toFixed(2);
+            }else{
+                usd.value = 'данный ошибочны!';
+            }
+
+        });
+
+      });
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const inputRub = document.querySelector('#rub'),
+//       inputUsd = document.querySelector('#usd');
+
+// inputRub.addEventListener('input', () => {
+//     const request = new XMLHttpRequest(); //запрос
+
+//     // request.open(method, url, async, login, pass); 
+//     request.open('GET', 'js/current.json');
+//     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+//     request.send();
+
+//     request.addEventListener('load', () => {
+//         if( request.status == 200) {
             
-            const data = JSON.parse(request.response);
-            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
-        }else{
-            inputUsd.value = 'что-то пошло не так';
-        }
-    });
+//             const data = JSON.parse(request.response);
+//             inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+//         }else{
+//             inputUsd.value = 'что-то пошло не так';
+//         }
+//     });
 
     // status
     // statusText
     // response
     // readyState
 
-});
+// });
