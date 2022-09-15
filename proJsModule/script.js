2813cd3273ff5940c0fb8897e3b06e280e7c69ea
Aliaksandr <sanya2798@gmail.com>
2022-09-15 17:56:11 +0300

    UP module 'array functions'

--- proJsModule/script.js
+++ proJsModule/script.js
@@ -69,3 +69,54 @@
 // Promise.race([test(1000),test(2000)]).then(() => {
 //     console.log('All');
 // });
+
+//Array functions
+//filter
+
+// const names = ['ivan', 'alex','katerina', 'logomanson'];
+
+// const shortNames = names.filter(function(name) {
+//     return name.length < 5;
+// });
+
+// console.log(shortNames);
+
+//map
+
+// const answers = ['ivan','annA', 'HellO'];
+
+// const result = answers.map((item) => item.toLowerCase());
+
+// console.log(result);
+
+//every/some
+
+// const some = [4, 'dwqe', 'fsaf'];
+
+// console.log(some.some((item) => typeof(item) === 'number')); // один элемент
+// console.log(some.every((item) => typeof(item) === 'number')); // все элементы
+
+//reduce
+
+// const arr = [4, 5, 6, 1, 3, 2];
+
+// const res = arr.reduce((sum, current) => sum + current, 3); // аргумент 3 начальное значение
+// console.log(res);
+
+// const fruit = ['apple', 'orange', 'lemon'];
+
+// const res = fruit.reduce ((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+// const obj = {
+//     ivan: 'persone',
+//     ann: 'persone',
+//     dog: 'animal',
+//     cat: 'animal'
+// };
+
+// const newArr = Object.entries(obj)
+// .filter(item => item[1] === 'persone')
+// .map(item => item[0]);
+
+// console.log(newArr);
